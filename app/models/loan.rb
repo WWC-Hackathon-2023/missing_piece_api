1cc726d84236e37c43fac1f2f3f5fe42544e0f4b
Melony Franchini <meltravelz@gmail.com>
2023-10-16 14:02:10 -0600

    feat: Add all model validations
    Co-authored-by: bisratlike <bisratlike@gmail.com>

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -3,5 +3,7 @@ class Loan < ApplicationRecord
   belongs_to :borrower, class_name: 'User', foreign_key: 'borrower_id'
   belongs_to :puzzle
 
+  validates_presence_of :owner_id, :borrower_id, :puzzle_id, :status, presence: true
+
   enum status: { 'Pending' => 0, 'Accepted' => 1, 'Denied' => 2, 'Closed' => 3 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_many :puzzles
   has_many :owner_loans, class_name: 'Loan', foreign_key: 'owner_id'
   has_many :borrower_loans, class_name: 'Loan', foreign_key: 'borrower_id'
+
+  validates_presence_of :full_name, :email, :zip_code, :phone_number
+  validates_numericality_of :zip_code
 end

1024cad211cc13471d1e7dd3714e61ff83ca0499
Melony Franchini <meltravelz@gmail.com>
2023-10-18 12:24:47 -0600

    fix: Update formatting on faker phone_number

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,13 @@ FactoryBot.define do
   factory :user do
     full_name { Faker::Name.name }
     email { Faker::Internet.safe_email }
-    zip_code { Faker::Number.number(digits: 5) } #we are using US zip codes as our example data - only 5 digits
-    phone_number { Faker::PhoneNumber.cell_phone.gsub(/(\d{3})(\d{3})(\d{4})/, '(\1) \2-\3') } #we format all phone numbers before saving them in the database
+
+    #we are using US zip codes for MVP: only 5 digits
+    zip_code { Faker::Number.number(digits: 5) }
+
+    #We are using US examples for the MVP: this will need to change for international usage
+    #we format all phone numbers before saving them in the database
+    phone_number { "(#{Faker::Number.number(digits: 3)}) #{Faker::Number.number(digits: 3)}-#{Faker::Number.number(digits: 4)}" }
     user_image_url { Faker::Internet.url(host: 'aws-s3') }
   end
 end

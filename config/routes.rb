7162901403d77c77e39848ecb3fcafe201f4de2d
Melony Franchini <meltravelz@gmail.com>
2023-10-18 21:51:49 -0600

    refactor: Add handrolled route for ease of use with team

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:show]
+      # resources :users, only: [:show] #for ease of understanding, we will skip resoruces for now
+      get '/users/:id', to: 'users#show'
+
     end
   end
 end

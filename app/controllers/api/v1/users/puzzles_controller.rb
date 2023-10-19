c5e09c19fa16e1bb72c6b96b39a65890b2c77b38
Melony Franchini <meltravelz@gmail.com>
2023-10-19 16:25:00 -0600

    test: complete user/puzzle#show happy path test

--- app/controllers/api/v1/users/puzzles_controller.rb
+++ app/controllers/api/v1/users/puzzles_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::Users::PuzzlesController < ApplicationController
+  
   def show
     render json: PuzzleSerializer.new(Puzzle.find(params[:puzzle_id]))
   end
--- spec/requests/api/v1/users/puzzles_request_spec.rb
+++ spec/requests/api/v1/users/puzzles_request_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe 'User/PuzzlesController' do
         user_1 = create(:user, id: 1)
         puzzle_1 = create(:puzzle, user: user_1)
         puzzle_2 = create(:puzzle, user: user_1)
-        puzzle_3 = create(:puzzle, user: user_1)
 
         get "/api/v1/users/#{user_1.id}/puzzles/#{puzzle_1.id}"
 
@@ -16,6 +15,21 @@ RSpec.describe 'User/PuzzlesController' do
         expect(response).to have_http_status(200)
 
         parsed_data = JSON.parse(response.body, symbolize_names: true)
+
+        expect(parsed_data).to be_a(Hash)
+        expect(parsed_data.keys).to eq([:data])
+        expect(parsed_data[:data]).to be_a(Hash)
+        expect(parsed_data[:data].keys).to eq([:id, :type, :attributes])
+
+        expect(parsed_data[:data][:attributes]).to be_a(Hash)
+        expect(parsed_data[:data][:attributes].keys).to eq([:user_id, :status, :title, :description, :total_pieces, :notes, :puzzle_image_url])
+        expect(parsed_data[:data][:attributes][:user_id]).to eq(puzzle_1.user_id)
+        expect(parsed_data[:data][:attributes][:status]).to eq(puzzle_1.status)
+        expect(parsed_data[:data][:attributes][:title]).to eq(puzzle_1.title) 
+        expect(parsed_data[:data][:attributes][:description]).to eq(puzzle_1.description)
+        expect(parsed_data[:data][:attributes][:total_pieces]).to eq(puzzle_1.total_pieces)
+        expect(parsed_data[:data][:attributes][:notes]).to eq(puzzle_1.notes)
+        expect(parsed_data[:data][:attributes][:puzzle_image_url]).to eq(puzzle_1.puzzle_image_url)
       end
     end
 

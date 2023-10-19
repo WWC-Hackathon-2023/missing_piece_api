0504335956876bf747a5f7adf2e28e2257682515
Melony Franchini <meltravelz@gmail.com>
2023-10-19 16:18:22 -0600

    feat: Add userspuzzles#show route

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       # resources :users, only: [:show] #for ease of understanding, we will skip resoruces for now
-      get '/users/:id', to: 'users#show'
-
+      get '/users/:user_id/puzzles/:puzzle_id', to: 'users/puzzles#show'
+      get '/users/:user_id', to: 'users#show'
     end
   end
 end

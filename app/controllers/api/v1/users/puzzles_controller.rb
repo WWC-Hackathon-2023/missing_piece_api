687828b8352e52725d6a449457714c88301b13fb
Melony Franchini <meltravelz@gmail.com>
2023-10-19 17:19:05 -0600

    fix: Remove commented out line

--- app/controllers/api/v1/users/puzzles_controller.rb
+++ app/controllers/api/v1/users/puzzles_controller.rb
@@ -3,8 +3,6 @@ class Api::V1::Users::PuzzlesController < ApplicationController
   def show
     user = User.find(params[:user_id])
     puzzle = user.puzzles.find(params[:puzzle_id])
-
-    # puzzle = Puzzle.find_by(id: params[:puzzle_id], user_id: params[:user_id])
     render json: PuzzleSerializer.new(puzzle)
   end
 end

4e2f14ed0f8f719d7f7a2a8d323ba5a2154289a7
Kemi Thomas <kemothom71@gmail.com>
2023-10-20 15:19:27 -0400

    refactored index method and have passing tests

--- app/controllers/api/v1/puzzles_controller.rb
+++ app/controllers/api/v1/puzzles_controller.rb
@@ -1,18 +1,16 @@
 class Api::V1::PuzzlesController < ApplicationController
 
   def index
-      
+
       zip_code = params[:zip_code]
 
-      @users = User.where(zip_code: zip_code) if zip_code.present?
-
-      if @users.present?
-
-        @puzzles = Puzzle.where(user_id: @users.pluck(:id))
-        render json: @puzzles
-      else
-        puzzles = []
-        render json: { error: "Puzzles not found in this area" }, status: :not_found
+      users = User.where(zip_code: zip_code) if zip_code.present?
+      
+      if users != []
+        puzzles = Puzzle.where(user_id: users.pluck(:id))
+        render json: PuzzleSerializer.new(puzzles)
+      elsif puzzles == [] || users = []
+        render json: { error: "Puzzles not found in this area" }, status: "404"
       end
   end
 

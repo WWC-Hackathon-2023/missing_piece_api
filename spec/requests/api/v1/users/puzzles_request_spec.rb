4933ec4945b6919d9c71ee5482baee9ab9256001
Melony Franchini <meltravelz@gmail.com>
2023-10-19 18:06:07 -0600

    test & feat: Add UserPuzzles#index endpoint with happy & sad path tests

--- spec/requests/api/v1/users/puzzles_request_spec.rb
+++ spec/requests/api/v1/users/puzzles_request_spec.rb
@@ -1,6 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe 'User/PuzzlesController' do
+  describe '#index' do
+    before(:each) do
+      @user_1 = create(:user, id: 1)
+
+      5.times do
+        create(:puzzle, user: @user_1)
+      end
+
+      @puzzle_1 = @user_1.puzzles[0]
+    end
+
+    context "when successful" do
+      it 'returns all puzzles that a user owns' do
+        get "/api/v1/users/#{@user_1.id}/puzzles"
+
+        expect(response).to have_http_status(200)
+
+        parsed_data = JSON.parse(response.body, symbolize_names: true)
+
+        expect(parsed_data).to be_a(Hash)
+        expect(parsed_data.keys).to eq([:data])
+        expect(parsed_data[:data]).to be_an(Array)
+        expect(parsed_data[:data][0].keys).to eq([:id, :type, :attributes])
+
+        expect(parsed_data[:data][0][:attributes]).to be_a(Hash)
+        expect(parsed_data[:data][0][:attributes].keys).to eq([:user_id, :status, :title, :description, :total_pieces, :notes, :puzzle_image_url])
+        expect(parsed_data[:data][0][:attributes][:user_id]).to eq(@puzzle_1.user_id)
+        expect(parsed_data[:data][0][:attributes][:status]).to eq(@puzzle_1.status)
+        expect(parsed_data[:data][0][:attributes][:title]).to eq(@puzzle_1.title) 
+        expect(parsed_data[:data][0][:attributes][:description]).to eq(@puzzle_1.description)
+        expect(parsed_data[:data][0][:attributes][:total_pieces]).to eq(@puzzle_1.total_pieces)
+        expect(parsed_data[:data][0][:attributes][:notes]).to eq(@puzzle_1.notes)
+        expect(parsed_data[:data][0][:attributes][:puzzle_image_url]).to eq(@puzzle_1.puzzle_image_url)
+      end
+    end
+
+    context "when NOT successful" do
+      it 'returns an error message when user_id is invalid' do
+        get "/api/v1/users/007/puzzles"
+
+        expect(response).to have_http_status(404)
+
+        parsed_error_data = JSON.parse(response.body, symbolize_names: true)
+
+        expect(parsed_error_data).to be_a(Hash)
+        expect(parsed_error_data.keys).to eq([:errors])
+        expect(parsed_error_data[:errors]).to be_an(Array)
+        expect(parsed_error_data[:errors][0].keys).to eq([:status, :title, :detail])
+        expect(parsed_error_data[:errors][0][:status]).to eq("404")
+        expect(parsed_error_data[:errors][0][:title]).to eq("ActiveRecord::RecordNotFound")
+        expect(parsed_error_data[:errors][0][:detail]).to eq("Couldn't find User with 'id'=007")
+      end
+    end
+  end
+
   describe '#show' do
     context "when successful" do
       it 'returns a single puzzle & its attributes' do

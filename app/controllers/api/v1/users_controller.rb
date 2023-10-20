0b1ee8f1d1c5dc1cf4dcaeb443a6ec2a62533730
Melony Franchini <meltravelz@gmail.com>
2023-10-19 22:52:02 -0600

    feat: Add route and controller for user#dashboard

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,5 +3,10 @@ class Api::V1::UsersController < ApplicationController
   def show
     render json: UserSerializer.new(User.find(params[:user_id]))
   end
+
+  def dashboard
+    user = User.find(params[:user_id])
+    render json: UserSerializer.new(user, params: { action: 'dashboard' })
+  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       # resources :users, only: [:show] #for ease of understanding, we will skip resoruces for now
-      get '/users/:user_id/puzzles/:puzzle_id', to: 'users/puzzles#show'
       get '/users/:user_id', to: 'users#show'
+      get '/users/:user_id/dashboard', to: 'users#dashboard'
+
+      get '/users/:user_id/puzzles/:puzzle_id', to: 'users/puzzles#show'
     end
   end
 end

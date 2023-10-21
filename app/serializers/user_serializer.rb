07a963a23ecb9d0894caf7d388d14e8f705da322
Melony Franchini <meltravelz@gmail.com>
2023-10-20 22:17:50 -0600

    feat: Attempt at adding puzzle info to #dashboard endpoint

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -8,15 +8,67 @@ class UserSerializer
              :phone_number,
              :user_image_url
 
-  # There are two ways to write this syntax for Proc
-  # OPTION 1: the value of xxxx_loans is an array of hashes
   attributes :owner_loans, if: proc { |record, params|
-    LoanSerializer.new(record.owner_loans) if params[:action] == 'dashboard'
+    if params[:action] == 'dashboard'
+      owner_loans = record.owner_loans.includes(:puzzle) # eager loading puzzles
+      # loans = record.owner_loans
+
+      owner_loans.map do |loan|      
+        puzzle = loan.puzzle
+        { 
+          # owner_loan: LoanSerializer.new(loan), 
+          loan_id: loan.id,
+          owner_id: loan.owner_id,
+          borrower_id: loan.borrower_id,
+          loan_status: loan.status,
+          loan_created_at: loan.created_at,
+          puzzle_id: loan.puzzle_id,
+          puzzle_image: puzzle.puzzle_image_url,
+          puzzle_title: puzzle.title,
+          puzzle_status: puzzle.status
+          # loan_puzzle_info: PuzzleSerializer.new(puzzle)
+        }
+      end
+    end
   }
 
   attributes :borrower_loans, if: proc { |record, params|
-    LoanSerializer.new(record.borrower_loans) if params[:action] == 'dashboard'
+    if params[:action] == 'dashboard'
+      borrower_loans = record.borrower_loans.includes(:puzzle) # eager loading puzzles
+
+      borrower_loans.map do |loan|      
+        puzzle = loan.puzzle
+        { 
+          # borrower_loan: LoanSerializer.new(loan), 
+          loan_id: loan.id,
+          owner_id: loan.owner_id,
+          borrower_id: loan.borrower_id,
+          loan_status: loan.status,
+          loan_created_at: loan.created_at,
+          puzzle_id: loan.puzzle_id,
+          puzzle_image: puzzle.puzzle_image_url,
+          puzzle_title: puzzle.title,
+          puzzle_status: puzzle.status
+          # loan_puzzle_info: PuzzleSerializer.new(puzzle)
+        }
+      end
+    end
   }
+
+end
+
+
+  #DRAFT ONE: 
+
+  # There are two ways to write this syntax for Proc
+  # OPTION 1: the value of xxxx_loans is an array of hashes
+  # attributes :owner_loans, if: proc { |record, params|
+  #   LoanSerializer.new(record.owner_loans) if params[:action] == 'dashboard'
+  # }
+
+  # attributes :borrower_loans, if: proc { |record, params|
+  #   LoanSerializer.new(record.borrower_loans) if params[:action] == 'dashboard'
+  # }
 
   # OPTION 2: the value of xxxx_loans is a hash with key of data that is an array of hashes
   # attributes :owner_loans, if: Proc.new { |record, params| params[:action] == 'dashboard' } do |object, params|
@@ -32,4 +84,3 @@ class UserSerializer
   # The Proc acts as a callable object that the serializer can use to determine whether the attribute should be included
   # or not. So, the Proc.new is used to properly encapsulate the conditional logic and return a boolean value, allowing
   # the serializer to determine whether to include the :borrower_loans attribute based on the value of params[:action].
-end

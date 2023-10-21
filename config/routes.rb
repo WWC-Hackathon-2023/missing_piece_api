0b6698f693d308e1fa3a81f85d9731862e088ca9
Melony Franchini <meltravelz@gmail.com>
2023-10-21 10:46:02 -0600

    fix: Update route from puzzles to /puzzles

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       # resources :users, only: [:show] #for ease of understanding, we will skip resoruces for now
-      put 'puzzles', to: 'puzzles#index'
+      put '/puzzles', to: 'puzzles#index'
 
       get '/users/:user_id', to: 'users#show'
       get '/users/:user_id/dashboard', to: 'users#dashboard'

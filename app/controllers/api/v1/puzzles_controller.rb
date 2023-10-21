c504e6f6c5f9d7cff7504c8e889b98d020ffcc19
Kemi Thomas <kemothom71@gmail.com>
2023-10-21 19:22:47 -0400

    remove syntax errors and pull from main

--- app/controllers/api/v1/puzzles_controller.rb
+++ app/controllers/api/v1/puzzles_controller.rb
@@ -1,17 +1,13 @@
-module Api
-  module V1
-    class PuzzlesController < ApplicationController
-      def index
-        zip_code = params[:zip_code]
-        users = User.where(zip_code:) if zip_code.present?
+class Api::V1::PuzzlesController < ApplicationController
+    def index
+      zip_code = params[:zip_code]
+      users = User.where(zip_code:) if zip_code.present?
 
-        if users != []
-          puzzles = Puzzle.where(user_id: users.pluck(:id))
-          render json: PuzzleSerializer.new(puzzles)
-        elsif puzzles == [] || users == []
-          render json: { error: "Puzzles not found in this area" }, status: "404"
-        end
+      if users != []
+        puzzles = Puzzle.where(user_id: users.pluck(:id))
+        render json: PuzzleSerializer.new(puzzles)
+      elsif puzzles == [] || users == []
+        render json: { error: "Puzzles not found in this area" }, status: "404"
       end
     end
-  end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,20 +1,10 @@
-module Api
-  module V1
-    class UsersController < ApplicationController
-      def show
-        render json: UserSerializer.new(User.find(params[:user_id]))
-      end
+class Api::V1::UsersController < ApplicationController
+  def show
+    render json: UserSerializer.new(User.find(params[:user_id]))
+  end
 
-<<<<<<< HEAD
-      def dashboard
-        user = User.find(params[:user_id])
-        render json: UserSerializer.new(user, params: { action: 'dashboard' })
-      end
-    end
-=======
   def dashboard
     dashboard = User.find(params[:user_id]).find_dashboard_info
     render json: DashboardSerializer.new(dashboard)
->>>>>>> 434c0354a110cda0a8db3935c807108c6e2c7276
   end
 end

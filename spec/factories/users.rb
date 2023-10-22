ae9a8d23d6c60c113557cbe78c84d7d90d7b56fe
Melony Franchini <meltravelz@gmail.com>
2023-10-22 00:00:43 -0600

    fix: Remove user_image_url from Users table

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,11 +4,19 @@ FactoryBot.define do
   factory :user do
     full_name { Faker::Name.name }
     email { Faker::Internet.email }
+    password { Faker::Alphanumeric.alphanumeric(number: 10, min_alpha: 3, min_numeric: 3) } 
+
+    # Set password_confirmation to the same value as password 
+    password_confirmation { |user| user.password } 
+
     # we are using US zip codes for MVP: only 5 digits
     zip_code { Faker::Number.number(digits: 5) }
+
     # We are using US examples for the MVP: this will need to change for international usage
     # We format all phone numbers before saving them in the database
     phone_number { "(#{Faker::Number.number(digits: 3)}) #{Faker::Number.number(digits: 3)}-#{Faker::Number.number(digits: 4)}" }
-    user_image_url { Faker::Internet.url(host: 'aws-s3') }
+    
+    # Decided not to use this and go with an icon on the FE repo
+    # user_image_url { Faker::Internet.url(host: 'aws-s3') }
   end
 end

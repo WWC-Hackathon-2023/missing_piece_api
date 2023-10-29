5279102f0be8984d27500cea96bf09d35624426b
Melony Franchini <meltravelz@gmail.com>
2023-10-29 11:36:21 -0600

    refactor: Puzzles#index action Oh YEA '

--- app/controllers/api/v1/puzzles_controller.rb
+++ app/controllers/api/v1/puzzles_controller.rb
@@ -2,12 +2,24 @@ class Api::V1::PuzzlesController < ApplicationController
   skip_before_action :set_current_user, only: [:index]
 
   def index
-    puzzles_in_zip_code = Puzzle.find_by_zip_code(params[:zip_code])
+    find_puzzles(params[:zip_code])
+    raise NoPuzzlesException if @puzzles_in_zip_code.empty?
+    render json: PuzzleSerializer.new(@puzzles_in_zip_code)
+  end
 
-    if puzzles_in_zip_code.any?
-      render json: PuzzleSerializer.new(puzzles_in_zip_code)
-    else
-      raise NoPuzzlesException
-    end
+  private 
+
+  def find_puzzles(zip_code)
+    @puzzles_in_zip_code = Puzzle.find_by_zip_code(zip_code)
   end
 end
+
+# Note to self: Saving this to remember process that lead me to final version:
+#def index
+  # puzzles_in_zip_code = Puzzle.find_by_zip_code(params[:zip_code])
+  # if @puzzles_in_zip_code.any?
+  #   render json: PuzzleSerializer.new(@puzzles_in_zip_code)
+  # else
+  #   raise NoPuzzlesException
+  # end
+# end

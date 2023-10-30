23fe01f4f95ee1294daf5a347a97fc28db8de422
Melony Franchini <meltravelz@gmail.com>
2023-10-30 09:47:19 -0600

    refactor: Sessions#create with helper method authenticate_user

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,20 +3,9 @@ class Api::V1::SessionsController < ApplicationController
   before_action :authorize, only: [:destroy]
 
   def create
-    if params[:email].present? && params[:password].present?
-      returning_user = User.find_by(email: params[:email])
-
-      # the & is called a "safe navigation" operator
-      # It prevent a "NoMethodError" from being raised when invoking a method on nil
-      if returning_user&.authenticate(params[:password])
-        session[:user_id] = returning_user.id
-        render json: UserSerializer.new(returning_user), status: :created
-      else
-        render json: { error: 'Invalid email or password' }, status: :unauthorized
-      end
-    else
-      render json: { error: 'Email and password are required' }, status: :unprocessable_entity
-    end
+    authenticate_user
+    session[:user_id] = @returning_user.id
+    render json: UserSerializer.new(@returning_user), status: :created
   end
 
   def destroy
@@ -28,5 +17,14 @@ class Api::V1::SessionsController < ApplicationController
   
   def authorize
     render json: { error: 'Not authorized' }, status: :unauthorized unless session[:user_id]
+  end
+
+  def authenticate_user
+    if params[:email].present? && params[:password].present?
+      @returning_user = User.find_by(email: params[:email])
+      raise InvalidAuthenticationException unless @returning_user&.authenticate(params[:password])
+    else 
+      raise MissingAuthenticationException
+    end
   end
 end

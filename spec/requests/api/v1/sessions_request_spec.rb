e508f0c35c763df2b78776c9de2612e7921e9535
Melony Franchini <meltravelz@gmail.com>
2023-10-30 09:46:27 -0600

    test: refactor & add sad path test to sessions_request_spec

--- spec/requests/api/v1/sessions_request_spec.rb
+++ spec/requests/api/v1/sessions_request_spec.rb
@@ -2,17 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'SessionsController' do
   describe '#create' do
+    before(:each) do
+      @user = User.create(
+        full_name: "Diana Puzzler",
+        password: "PuzzleQueen1",
+        password_confirmation: "PuzzleQueen1",
+        email: "dpuzzler@myemail.com", # The Users#create will format the email in this way before it is saved to the db
+        zip_code: 12345,
+        phone_number: "(555) 000-9999" # The Users#create will format the phone_number in this way before it is saved to the db
+      )
+    end
+
     context 'when successful' do
       it 'logs in a user' do
-        user = User.create(
-          full_name: "Diana Puzzler",
-          password: "PuzzleQueen1",
-          password_confirmation: "PuzzleQueen1",
-          email: "dpuzzler@myemail.com",
-          zip_code: 12345,
-          phone_number: 5500009999
-        )
-
         login_data = {
           email: "dpuzzler@myemail.com",
           password: "PuzzleQueen1"
@@ -22,21 +24,14 @@ RSpec.describe 'SessionsController' do
         post "/api/v1/login", headers:, params: JSON.generate(login_data)
 
         expect(response).to have_http_status(201)
-        expect(session[:user_id]).to eq(user.id)
+        parsed_data = JSON.parse(response.body, symbolize_names: true)
+
+        expect(session[:user_id]).to eq(@user.id)
       end
     end
 
     context 'when NOT successful' do
       it 'cannot log in a user with an incorrect password' do
-        user = User.create(
-          full_name: "Diana Puzzler",
-          password: "PuzzleQueen1",
-          password_confirmation: "PuzzleQueen1",
-          email: "dpuzzler@myemail.com",
-          zip_code: 12345,
-          phone_number: 5550009999
-        )
-
         login_data = {
           email: "dpuzzler@myemail.com",
           password: "Queen_of_Puzzles"
@@ -46,12 +41,38 @@ RSpec.describe 'SessionsController' do
         post "/api/v1/login", headers:, params: JSON.generate(login_data)
 
         expect(response).to have_http_status(401)
-
         parsed_error_data = JSON.parse(response.body, symbolize_names: true)
 
         expect(parsed_error_data).to be_a(Hash)
-        expect(parsed_error_data.keys).to eq([:error])
-        expect(parsed_error_data[:error]).to eq("Invalid email or password")
+        expect(parsed_error_data.keys).to eq([:errors])
+        expect(parsed_error_data[:errors]).to be_an(Array)
+        expect(parsed_error_data[:errors][0]).to be_a(Hash)
+        expect(parsed_error_data[:errors][0].keys).to eq([:status, :title, :detail])
+        expect(parsed_error_data[:errors][0][:status]).to eq("401")
+        expect(parsed_error_data[:errors][0][:title]).to eq("InvalidAuthenticationException")
+        expect(parsed_error_data[:errors][0][:detail]).to eq("Invalid email or password.")
+      end
+
+      it 'cannot log in a user with a missing email and password' do
+        login_data = {
+          email: nil,
+          password: nil
+        }
+
+        headers = { 'CONTENT_TYPE' => 'application/json' }
+        post "/api/v1/login", headers:, params: JSON.generate(login_data)
+
+        expect(response).to have_http_status(401)
+        parsed_error_data = JSON.parse(response.body, symbolize_names: true)
+
+        expect(parsed_error_data).to be_a(Hash)
+        expect(parsed_error_data.keys).to eq([:errors])
+        expect(parsed_error_data[:errors]).to be_an(Array)
+        expect(parsed_error_data[:errors][0]).to be_a(Hash)
+        expect(parsed_error_data[:errors][0].keys).to eq([:status, :title, :detail])
+        expect(parsed_error_data[:errors][0][:status]).to eq("401")
+        expect(parsed_error_data[:errors][0][:title]).to eq("MissingAuthenticationException")
+        expect(parsed_error_data[:errors][0][:detail]).to eq("Email and password are required.")
       end
     end
   end

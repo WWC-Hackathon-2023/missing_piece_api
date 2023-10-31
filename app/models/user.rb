df322b9e202b6510fc9f638cc165dcae781c91ac
Melony Franchini <meltravelz@gmail.com>
2023-10-31 15:06:14 -0600

    refactor: #find_dashboard_info & added test

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,54 +16,11 @@ class User < ApplicationRecord
     self.phone_number = format_phone_number
   end
 
-  # This method needs a HUGE refactor:
   def find_dashboard_info
-    owner_loans_with_puzzle = owner_loans.includes(:puzzle).where(status: [0, 1])
-    borrower_loans_with_puzzle = borrower_loans.includes(:puzzle).where(status: [0, 1])
-
-    dashboard_info = {
-      id:,
-      user_info: {
-        full_name:,
-        email:,
-        zip_code:,
-        phone_number:
-      },
-      owner_loans: [],
-      borrower_loans: []
-    }
-
-    owner_loans_with_puzzle.each do |loan|
-      puzzle = loan.puzzle
-      dashboard_info[:owner_loans] << {
-        loan_id: loan.id,
-        owner_id: loan.owner_id,
-        borrower_id: loan.borrower_id,
-        loan_status: loan.status,
-        loan_created_at: loan.created_at,
-        puzzle_id: loan.puzzle_id,
-        puzzle_image_url: puzzle.puzzle_image_url,
-        puzzle_title: puzzle.title,
-        puzzle_status: puzzle.status
-      }
-    end
-
-    borrower_loans_with_puzzle.each do |loan|
-      puzzle = loan.puzzle
-      dashboard_info[:borrower_loans] << {
-        loan_id: loan.id,
-        owner_id: loan.owner_id,
-        borrower_id: loan.borrower_id,
-        loan_status: loan.status,
-        loan_created_at: loan.created_at,
-        puzzle_id: loan.puzzle_id,
-        puzzle_image_url: puzzle.puzzle_image_url,
-        puzzle_title: puzzle.title,
-        puzzle_status: puzzle.status
-      }
-    end
-
-    OpenStruct.new(dashboard_info)
+    dashboard = create_dashboard_with_user_info
+    updated_dashboard = find_owner_loans_with_puzzle(dashboard)
+    complete_dashboard = find_borrower_loans_with_puzzle(updated_dashboard)
+    OpenStruct.new(complete_dashboard)
   end
 
   private
@@ -74,5 +31,61 @@ class User < ApplicationRecord
 
   def format_phone_number
     phone_number.insert(0, '(').insert(4, ')').insert(5, " ").insert(9, "-")
+  end
+
+  def create_dashboard_with_user_info
+    {
+      id: id,
+      user_info: {
+        full_name: full_name,
+        email: email,
+        zip_code: zip_code,
+        phone_number: phone_number
+      },
+      owner_loans: [],
+      borrower_loans: []
+    }
+  end
+
+  def find_owner_loans_with_puzzle(dashboard)
+    owner_loans_with_puzzle = owner_loans.includes(:puzzle).where(status: [0, 1])
+
+    dashboard[:owner_loans] = owner_loans_with_puzzle.map do |loan|
+      puzzle = loan.puzzle
+      {
+        loan_id: loan.id,
+        owner_id: loan.owner_id,
+        borrower_id: loan.borrower_id,
+        loan_status: loan.status,
+        loan_created_at: loan.created_at,
+        puzzle_id: loan.puzzle_id,
+        puzzle_image_url: puzzle.puzzle_image_url,
+        puzzle_title: puzzle.title,
+        puzzle_status: puzzle.status
+      }
+    end
+
+    dashboard
+  end
+
+  def find_borrower_loans_with_puzzle(dashboard)
+    borrower_loans_with_puzzle = borrower_loans.includes(:puzzle).where(status: [0, 1])
+
+    dashboard[:borrower_loans] = borrower_loans_with_puzzle.map do |loan|
+      puzzle = loan.puzzle
+      {
+        loan_id: loan.id,
+        owner_id: loan.owner_id,
+        borrower_id: loan.borrower_id,
+        loan_status: loan.status,
+        loan_created_at: loan.created_at,
+        puzzle_id: loan.puzzle_id,
+        puzzle_image_url: puzzle.puzzle_image_url,
+        puzzle_title: puzzle.title,
+        puzzle_status: puzzle.status
+      }
+    end
+
+    dashboard
   end
 end

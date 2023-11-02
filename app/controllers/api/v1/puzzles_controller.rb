3f238ec93861400d49529614db610380e75b1c5e
Melony Franchini <meltravelz@gmail.com>
2023-11-02 12:10:39 -0600

    refactor: add before_action in puzzles_controller"

--- app/controllers/api/v1/puzzles_controller.rb
+++ app/controllers/api/v1/puzzles_controller.rb
@@ -1,16 +1,17 @@
 class Api::V1::PuzzlesController < ApplicationController
   skip_before_action :set_current_user, only: [:index]
+  before_action :find_puzzles, only: [:index]
 
   def index
-    find_puzzles(params[:zip_code])
+    # find_puzzles(params[:zip_code]) 
     raise NoPuzzlesFoundException if @puzzles_in_zip_code.empty?
     render json: PuzzleSerializer.new(@puzzles_in_zip_code)
   end
 
   private
 
-  def find_puzzles(zip_code)
-    @puzzles_in_zip_code = Puzzle.find_by_zip_code(zip_code)
+  def find_puzzles
+    @puzzles_in_zip_code = Puzzle.find_by_zip_code(params[:zip_code])
   end
 end
 

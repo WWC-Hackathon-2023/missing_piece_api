c5c361ca2bd56fb81126603458dacd8edfb05174
Melony Franchini <meltravelz@gmail.com>
2023-11-02 22:22:08 -0600

    feat: add scope to puzzle model (commented out) as example

--- app/models/puzzle.rb
+++ app/models/puzzle.rb
@@ -7,6 +7,15 @@ class Puzzle < ApplicationRecord
 
   enum status: { 'Available' => 0, 'Pending' => 1, 'Not Available' => 2, "Permanently Removed" => 3 }
 
+  # scope is chainable (easily combined with other methods)
+  # if it's a commonly used query & you want to make it easily chainable with other query conditions, using scope is a good choice.
+  # scope :find_by_zip_code, ->(zip_code) {
+  #   joins(:user)
+  #     .where(users: { zip_code: zip_code })
+  #     .where.not(status: 3)
+  # }
+
+  # if the query is highly specific or needs complex, dynamic conditions, leaving it as a class method might be more appropriate.
   def self.find_by_zip_code(zip_code)
     joins(:user)
       .where(users: { zip_code: zip_code })
